e568b639590ec284bb4d8f6ce8a3d51025a5b63b
hassangamal <gamalhassan807@yahoo.com>
2018-07-06 23:56:26 +0200

    update query

--- database.py
+++ database.py
@@ -1,4 +1,4 @@
-# pick random word
+#!/usr/bin/env python3
 
 import psycopg2
 
@@ -7,19 +7,19 @@ def question_one():
     conn = psycopg2.connect("dbname=news user=postgres"
                             " password=hassan host=localhost")
     cur = conn.cursor()
-    cur.execute("select title , count(*) as num_view "
-                "from  articles inner join log "
-                "on log.path like concat('%', articles.slug, '%') "
-                "where log.status like '200%'"
+    cur.execute("SELECT title, "
+                "count(*) AS num_view "
+                "from  articles "
+                "inner join log on log.path = concat('/article/', articles.slug) "
+                "where log.status ='200 OK'"
                 "group by articles.title "
                 "order by num_view desc "
-                "limit 3")
+                "limit 3"
+                )
     rows = cur.fetchall()
     print("1. What are the most popular three articles of all time?")
-    idx = 1
     for row in rows:
-        print(idx, ' - ', row[0], ' -> ', row[1])
-        idx += 1
+        print(row[0], ' -- ', row[1], ' views')
     conn.commit()
     cur.close()
     conn.close()
@@ -29,21 +29,22 @@ def question_two():
     conn = psycopg2.connect("dbname=news user=postgres"
                             " password=hassan host=localhost")
     cur = conn.cursor()
-    cur.execute("select authors.name , count(*) as num_view "
-                "from  authors inner join articles "
-                "on authors.id=articles.author "
-                "inner join log "
-                "on log.path like concat('%', articles.slug, '%') "
-                "where log.status like '200%'"
+    cur.execute("select authors.name , "
+                "count(*) as num_view "
+                "from  authors "
+                " inner join articles on authors.id=articles.author "
+                "inner join log on log.path = concat('/article/', articles.slug) "
+                "where log.status ='200 OK' "
                 "group by authors.name "
                 "order by num_view desc "
-                "limit 3")
+                "limit 3"
+                )
     rows = cur.fetchall()
     print("2. Who are the most popular article authors of all time?")
-    idx = 1
+
     for row in rows:
-        print(idx, ' - ', row[0], ' -> ', row[1])
-        idx += 1
+        print(row[0], ' -- ', row[1], ' views')
+
     conn.commit()
     cur.close()
     conn.close()
@@ -55,31 +56,33 @@ def question_third():
                             " password=hassan host=localhost")
     cur = conn.cursor()
 
-    cur.execute(" create view dataerror as "
-                "select substring(cast(log.time as text), 0, 11) as date , count(log.status) as error from log "
+    cur.execute("create or replace view dataerror as "
+                "select substring(cast(log.time as text), 0, 11) as date, "
+                "count(log.status) as error "
+                "from log "
                 "where log.status like '%404%' "
                 "group by date "
                 )
-    cur.execute(" create view alldata as "
+    cur.execute("create or replace view  alldata as "
                 "select substring(cast(log.time as text), 0, 11) as date , count(log.status) as all from log "
                 "group by date "
                 )
-    cur.execute("create view final_query as select alldata.date, "
+    cur.execute("create or replace view final_query as "
+                "select alldata.date, "
                 "((dataerror.error / alldata.all::float)*100) as all_error "
-                "from alldata inner "
-                "join dataerror on "
-                "alldata.date=dataerror.date "
+                "from alldata "
+                "inner join dataerror on alldata.date=dataerror.date "
                 )
 
-    cur.execute(" select date,all_error from final_query where all_error>1;"
-
+    cur.execute(" select date, "
+                "all_error "
+                "from final_query "
+                "where all_error>1"
                 )
     rows = cur.fetchall()
     print("3-On which days did more than 1% of requests lead to errors?")
-    idx = 1
     for row in rows:
-        print(idx, ' - ', row[0], '->', row[1])
-        idx += 1
+        print(row[0], ' -- ',round(row[1], 2), '%  views')
     conn.commit()
     cur.close()
     conn.close()
